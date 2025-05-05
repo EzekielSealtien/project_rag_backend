6c8245b4995af3a1f156b69e609cabb77eaccc9d
EzekielSealtien <EzekielTakam@teccart.online>
2025-05-05 03:54:33 -0400

    Update chatbot.py

--- backend/Functions_/chatbot.py
+++ backend/Functions_/chatbot.py
@@ -19,7 +19,7 @@ def get_response_from_model(context,question):
     parser = StrOutputParser()
     template = """
     Answer the question based on the context below. If you can't answer the question, 
-    reply "Téléversez d'abord votre document,Ensuite je pourrais vous répondre.".
+    reply "Veuillez d'abord téléverser votre document, je pourrai ensuite répondre à vos questions.".
     Context: {context}
     Question: {question}
     """
